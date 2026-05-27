7d2e46e9cdca67466fd44e3f348c88be563cfa25
cesarleyto <72002673+cesarleyto@users.noreply.github.com>
2026-05-26 18:32:25 -0600

    Update content.config.ts

--- src/content.config.ts
+++ src/content.config.ts
@@ -4,12 +4,13 @@ import { z } from 'astro/zod';
 
 const blog = defineCollection({
     loader: glob({ base: './src/content/blog', pattern: '**/*.md' }),
-    schema: z.object({
+    // Convertimos el schema en una función que recibe ({ image })
+    schema: ({ image }) => z.object({
         title: z.string(),
         excerpt: z.string(),
         pubDate: z.coerce.date(),
         category: z.enum(['Síntomas', 'Diagnósticos', 'Testimonios', 'Vida Académica']).optional().default('Síntomas'),
-        image: z.string().optional(), // ¡NUEVO: Dejamos pasar la imagen!
+        image: image().optional(), // ¡Este es el traductor mágico de Astro!
     }),
 });
 
