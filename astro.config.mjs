e46b3a7c76d95a12245539d8346a7c55f2069523
cesarleyto <cezsar@gmail.com>
2026-05-28 20:00:20 -0600

    Eliminar adaptador de Netlify y limpiar astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,20 +2,19 @@
 import { defineConfig } from "astro/config";
 import Icons from "unplugin-icons/vite";
 import tailwindcss from "@tailwindcss/vite";
-import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://astro-elementary.netlify.app/",
-	base: "/",
-	output: "static",
-	vite: {
-		plugins: [
-			tailwindcss(),
-			Icons({
-				compiler: "astro",
-			}),
-		],
-	},
-	adapter: netlify(),
+    site: 'https://draaniracastro.com',
+    output: "static", // Esto le dice a Astro que construya HTML puro (perfecto para GitHub Pages)
+    
+    // Mantenemos tus configuraciones visuales intactas
+    vite: {
+        plugins: [
+            tailwindcss(),
+            Icons({
+                compiler: "astro",
+            }),
+        ],
+    }
 });
