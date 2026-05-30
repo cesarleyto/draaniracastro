e1442d100e1c0b7abcb1877736f4bb1fc6733cfe
cesarleyto <cezsar@gmail.com>
2026-05-29 22:41:43 -0600

    Agregado sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,18 +3,24 @@ import { defineConfig } from "astro/config";
 import Icons from "unplugin-icons/vite";
 import tailwindcss from "@tailwindcss/vite";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://draaniracastro.com',
-    output: "static", // Esto le dice a Astro que construya HTML puro (perfecto para GitHub Pages)
-    
-    // Mantenemos tus configuraciones visuales intactas
-    vite: {
-        plugins: [
-            tailwindcss(),
-            Icons({
-                compiler: "astro",
-            }),
-        ],
-    }
+  site: 'https://draaniracastro.com',
+
+  // Esto le dice a Astro que construya HTML puro (perfecto para GitHub Pages)
+  output: "static",
+
+  // Mantenemos tus configuraciones visuales intactas
+  vite: {
+      plugins: [
+          tailwindcss(),
+          Icons({
+              compiler: "astro",
+          }),
+      ],
+  },
+
+  integrations: [sitemap()]
 });
