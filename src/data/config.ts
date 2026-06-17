f7a1c93a58c0c35b39ae3902117181af666b1099
cesarleyto <cezsar@gmail.com>
2026-06-16 20:30:32 -0600

    Actualización

--- src/data/config.ts
+++ src/data/config.ts
@@ -1,14 +1,14 @@
 // Global variables
 export const siteLang = "es-MX";
 export const siteCurrency = "MXN";
-export const siteName = "Dra. Anira Castro";
-export const siteSlogan = "Gastroenterología Pediátrica";
+export const siteName = "Dra. Anira Castro;
+export const siteSlogan = "Gastroenterología Pediátrica. Cuidamos la pancita de tus pequeños.";
 export const themeColor = "#ffffff"; // brand-red
 export const unitSystem = "metric";
 
 export const phone = {
-    href: "tel:+522299840802",
-    label: "229 984 0802",
+    href: "tel:+525514817906",
+    label: "55 1481 7906",
 };
 
 export const email = {
